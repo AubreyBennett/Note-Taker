49ffae54c2786f512a5aede975385b7246035d12
Aubrey Bennett <27aubreyb@gmail.com>
2021-03-05 15:54:49 -0500

    updated code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uuid = require('uuid').v4;
 
 // Sets up the Express App
 const app = express();
@@ -15,16 +16,41 @@ app.use(express.static('public'));
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
-
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 // GET API's
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => {
+    const notes = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
+    res.json(JSON.parse(notes));
+});
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.post('/api/notes', (req, res) => {
     let newNote = req.body;
+    newNote.id = uuid();
+    // read the file
+    let notes = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
+    // parse the string
+    notes = JSON.parse(notes);
+    // push new note
     notes.push(newNote);
+    // write to the file (stringify)
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
+    // send a client a successful response (res.json(notes))
+    res.json(notes);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    let notes = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
+    notes = JSON.parse(notes);
+    notes = notes.filter(function(note) {
+        if (req.params.id != note.id)
+        return true;
+        else return false;
+    });
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
+    res.json(notes);
 });
 
 // Starts the server to begin listening
